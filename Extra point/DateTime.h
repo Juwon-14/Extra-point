2534d041327f565105fe3cc833f1acd5670ed649
Juwon-14 <wndnjs9127@gmail.com>
2024-12-13 13:14:03 -0800

    Update DateTime.h

--- Extra point/DateTime.h
+++ Extra point/DateTime.h
@@ -13,42 +13,30 @@
 #include <sstream>
 #include <iomanip>
 
-class Time {
-public:
-    int hour, minute, second;
-    int timeZoneOffset;
-
-    Time(int h = 0, int m = 0, int s = 0, int tz = 0);
-    void addSeconds(int sec);
-    void adjustTimeZone(int newOffset);
-};
-
-class Date {
-public:
+class DateTime {
+private:
     int year, month, day;
+    int hour, minute, second;
+    int timeZoneOffset; 
 
-    Date(int y = 1970, int m = 1, int d = 1);
+public:
+    DateTime(int y = 1970, int m = 1, int d = 1, int h = 0, int min = 0, int sec = 0, int tz = 0);
+
+    void addSeconds(int sec);
+    void addMinutes(int min);
+    void addHours(int hrs);
+    void addDays(int days);
+
+    void adjustTimeZone(int newOffset);
+    void applyDaylightSaving(int startMonth, int startDay, int endMonth, int endDay);
+
     bool isLeapYear() const;
     int daysInMonth() const;
-    void addDays(int days);
     std::string dayOfWeek() const;
+
+    std::string format(const std::string& formatString) const;
+    std::string serialize() const;
+    static DateTime deserialize(const std::string& serializedString);
 };
 
-class DateTime {
-public:
-    Date date;
-    Time time;
-    
-    DateTime(int y = 1970, int m = 1, int d = 1, int h = 0, int min = 0, int sec = 0, int tz = 0);
-        void addSeconds(int sec);
-        void addMinutes(int min);
-        void addHours(int hrs);
-        void addDays(int days);
-        void adjustTimeZone(int newOffset);
-        void applyDaylightSaving(int startMonth, int startDay, int endMonth, int endDay);
-        std::string format(const std::string& formatString) const;
-        std::string serialize() const; // Serialize DateTime to string
-        static DateTime deserialize(const std::string& serializedString); // Deserialize from string
-    };
-
-    #endif
+#endif
