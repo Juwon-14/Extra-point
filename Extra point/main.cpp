1fb1bac3811911c6df6f650d1c29f69a4e40c770
Juwon Hong <wndnjs9127@gmail.com>
2024-12-12 19:01:52 -0800

    Initial Commit

---
+++ Extra point/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Extra point
+//
+//  Created by 홍주원 on 12/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
