48b0d1329a37acc4f8c207bc1799c7af36542ce5
Juwon-14 <wndnjs9127@gmail.com>
2024-12-13 14:42:18 -0800

    Add files via upload

---
+++ Extra point/Time.h
@@ -0,0 +1,30 @@
+//
+//  Time.h
+//  Extra point
+//
+//  Created by Juwon Hong on 12/13/24.
+//
+
+// Time.h
+#ifndef TIME_H
+#define TIME_H
+
+class Time {
+protected:
+    int hour, minute, second;
+
+public:
+    Time(int h = 0, int m = 0, int s = 0);
+
+    void addSeconds(int sec);
+    void addMinutes(int min);
+    void addHours(int hrs);
+
+    int getHour() const;
+    int getMinute() const;
+    int getSecond() const;
+
+    void normalize();
+};
+
+#endif
